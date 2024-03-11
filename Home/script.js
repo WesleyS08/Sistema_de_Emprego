d40f494220ea1b0c536dda9a760168091e144be8
Pedro Borges de Jesus <pedro.borges.jesus@gmail.com>
2024-03-11 11:49:41 -0300

    Área de título concluída; Área de anúncios iniciada

---
+++ Home/script.js
@@ -0,0 +1,23 @@
+const titulo = document.querySelector("#tituloAutomatico");
+const text1 = "eu futuro começa aqui!";
+const interval = 200; // variável do tempo de digitação
+
+// método para fazer o título autodigitável
+function showText(titulo, text1, interval){
+    let char = text1.split("").reverse(); // esta variável está recebendo um array de todos caracteres do texto (ao inverso)
+    
+    // função responsável pela inserção de letras no título   
+    let typer = setInterval(() => {
+
+        // este if verifica se todos os caractéres já foram digitados (pois eles são removidos do array a medida que são exibidos)
+        if(!char.length){
+            return clearInterval(typer); // encerrando a função
+        }
+
+        let next = char.pop(); // variável recebendo e removendo a última letra do array
+        titulo.innerHTML += next; // adicionando a última letra ao título
+
+    }, interval)
+}
+
+showText(titulo, text1, interval);
