441d192da8c784c8cb1490bed06fa66aa2886c51
davi <davibritojunior1@gmail.com>
2024-03-23 19:02:25 -0300

    Alterações feitas no script

--- docs/Modelos do banco de dados/Script_de_criação.sql
+++ docs/Modelos do banco de dados/Script_de_criação.sql
@@ -102,7 +102,7 @@ ENGINE = InnoDB;
 -- Table `SIAS`.`Tb_Resultados`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `SIAS`.`Tb_Resultados` (
-  `Tb_Questionarios_ID` INT NOT NULL AUTO_INCREMENT,
+  `Tb_Questionarios_ID` INT NOT NULL,
   `Tb_Candidato_CPF` VARCHAR(12) NOT NULL,
   `Nota` INT NULL,
   `Quantidade_de_Acertos` INT NULL,
@@ -149,7 +149,7 @@ ENGINE = InnoDB;
 -- Table `SIAS`.`Tb_Vagas`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `SIAS`.`Tb_Vagas` (
-  `Tb_Anuncios_Id` INT NOT NULL AUTO_INCREMENT,
+  `Tb_Anuncios_Id` INT NOT NULL,
   `Tb_Empresa_CNPJ` VARCHAR(14) NOT NULL,
   `Estados` VARCHAR(45) NULL,
   `Data_de_Termino` DATETIME NULL,
@@ -173,7 +173,7 @@ ENGINE = InnoDB;
 -- Table `SIAS`.`Tb_Inscricoes`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `SIAS`.`Tb_Inscricoes` (
-  `Tb_Vagas_Tb_Anuncios_Id` INT NOT NULL AUTO_INCREMENT,
+  `Tb_Vagas_Tb_Anuncios_Id` INT NOT NULL,
   `Tb_Vagas_Tb_Empresa_CNPJ` VARCHAR(14) NOT NULL,
   `Tb_Candidato_CPF` VARCHAR(12) NOT NULL,
   `Data_de _Inscricao` DATETIME NULL,
@@ -214,7 +214,7 @@ ENGINE = InnoDB;
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `SIAS`.`Tb_Recomendacoes` (
   `Tb_Candidato_CPF` VARCHAR(12) NOT NULL,
-  `Tb_Cursos_Id` INT NOT NULL AUTO_INCREMENT,
+  `Tb_Cursos_Id` INT NOT NULL,
   PRIMARY KEY (`Tb_Candidato_CPF`, `Tb_Cursos_Id`),
   INDEX `fk_Tb_Candidato_has_Tb_Cursos_Tb_Cursos1_idx` (`Tb_Cursos_Id` ASC) ,
   INDEX `fk_Tb_Candidato_has_Tb_Cursos_Tb_Candidato1_idx` (`Tb_Candidato_CPF` ASC) ,
