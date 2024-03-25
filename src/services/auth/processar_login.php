d6179ab712421db457021f9797e8fab51243c248
Wesley <113936684+WesleyS08@users.noreply.github.com>
2024-03-25 15:23:27 -0300

    Update processar_login.php

--- src/services/auth/processar_login.php
+++ src/services/auth/processar_login.php
@@ -22,10 +22,16 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         } else {
             if (mysqli_num_rows($result_candidato) > 0) {
                 $_SESSION['tipo_usuario'] = 'candidato';
+                // Consulta para obter o nome do usuário
+                $row = mysqli_fetch_assoc($result_candidato);
+                $_SESSION['nome_usuario'] = $row['Nome']; // Supondo que o nome do usuário está na coluna 'Nome'
                 header("Location: pagina_do_candidato.php");
                 exit;
             } elseif (mysqli_num_rows($result_empresa) > 0) {
                 $_SESSION['tipo_usuario'] = 'empresa';
+                // Consulta para obter o nome do usuário
+                $row = mysqli_fetch_assoc($result_empresa);
+                $_SESSION['nome_usuario'] = $row['Nome']; // Supondo que o nome do usuário está na coluna 'Nome'
                 header("Location: ../../../../../HomeRecrutador/homeRecrutador.php");
                 exit;
             } else {
