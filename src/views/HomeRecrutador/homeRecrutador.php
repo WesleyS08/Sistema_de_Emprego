c198cc73b390d554e716c481403e2c292a99c476
Wesley <113936684+WesleyS08@users.noreply.github.com>
2024-04-02 06:56:35 -0300

    fix: Corrige esquema de cores tipo de vaga do recrutador
    - usando o switch para passar cases de cada tipo de vaga
    - definindo ícones e cores correspondentes

--- src/views/HomeRecrutador/homeRecrutador.php
+++ src/views/HomeRecrutador/homeRecrutador.php
@@ -109,15 +109,31 @@ $tokenSession = isset($_SESSION['token_session']) ? $_SESSION['token_session'] :
                                 echo '<img src="../../../imagens/people.svg"></img>';
                                 echo '<small class="qntdAcessos">28</small>';
                                 echo '</div>';
+                            
                                 echo '<header>';
-                                echo '<img src="../../../imagens/estagio.svg">';
-
-                                if (isset($row["Categoria"])) {
-                                    echo '<label class="tipoVaga" style="color:#191970">' . $row["Categoria"] . '</label>';
-                                } else {
-                                    echo '<label class="tipoVaga" style="color:#191970">Categoria não definida</label>';
+                                switch ($row["Categoria"]) {
+                                    case "CLT":
+                                        echo '<img src="../../../imagens/clt.svg">';
+                                        echo '<label class="tipoVaga" style="color:#008B8B">' . $row["Categoria"] . '</label>';
+                                        break;
+                                    case "Estágio":
+                                        echo '<img src="../../../imagens/estagio.svg">';
+                                        echo '<label class="tipoVaga" style="color:#191970">' . $row["Categoria"] . '</label>';
+                                        break;
+                                    case "Jovem Aprendiz":
+                                        echo '<img src="../../../imagens/estagio.svg">';
+                                        echo '<label class="tipoVaga" style="color:#EC6809">' . $row["Categoria"] . '</label>';
+                                        break;
+                                    case "PJ":
+                                        echo '<img src="../../../imagens/pj.svg">';
+                                        echo '<label class="tipoVaga" style="color:#800080">' . $row["Categoria"] . '</label>';
+                                        break;
+                                    default:
+                                        echo '<label class="tipoVaga" style="color:#191970">Categoria não definida</label>';
+                                        break;
                                 }
-
+                            
+                                echo '</header>';
                                 echo '</header>';
                                 echo '<section>';
                                 echo '<h3 class="nomeVaga">' . (isset($row["Titulo"]) ? $row["Titulo"] : "Título não definido") . '</h3>';
