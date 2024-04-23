c9d58bf47982e5d7ae7f0e90d0ae2e72a1f55ab0
Wesley <113936684+WesleyS08@users.noreply.github.com>
2024-04-23 16:27:19 -0300

    fix: passando o estilo para o btn

--- src/views/EditarPerfilRecrutador/editarPerfilRecrutador.php
+++ src/views/EditarPerfilRecrutador/editarPerfilRecrutador.php
@@ -74,7 +74,8 @@ if ($result && mysqli_num_rows($result) > 0) {
         <label for="check" class="menuBtn">
             <img src="../../../imagens/menu.svg">
         </label>
-        <a href="../HomeRecrutador/homeRecrutador.php"><img id="logo" src="../../assets/images/logos_empresa/logo_sias.png"></a>
+        <a href="../HomeRecrutador/homeRecrutador.php"><img id="logo"
+                src="../../assets/images/logos_empresa/logo_sias.png"></a>
         <button class="btnModo"><img src="../../../imagens/moon.svg"></button>
         <ul>
             <li><a href="../CriarVaga/criarVaga.php">Anunciar</a></li>
@@ -89,28 +90,34 @@ if ($result && mysqli_num_rows($result) > 0) {
             <h2>Editar Perfil</h2>
         </div>
         <div class="divEdicaoPerfil">
-            <form method="post" action="../../../src/services/Perfil/PerfilEmpresa.php?id=<?php echo $idPessoa; ?>" autocomplete="off" enctype="multipart/form-data">
+            <form method="post" action="../../../src/services/Perfil/PerfilEmpresa.php?id=<?php echo $idPessoa; ?>"
+                autocomplete="off" enctype="multipart/form-data">
 
 
 
                 <div class="divBackgroundImg">
                     <div class="btnEditarFundo">
-                        <lord-icon src="https://cdn.lordicon.com/wuvorxbv.json" trigger="hover" stroke="bold" colors="primary:#f5f5f5,secondary:#f5f5f5" style="width:34px;height:34px">
+                        <lord-icon src="https://cdn.lordicon.com/wuvorxbv.json" trigger="hover" stroke="bold"
+                            colors="primary:#f5f5f5,secondary:#f5f5f5" style="width:34px;height:34px">
                         </lord-icon>
                     </div>
-                    <input type="file" id="fundo_upload" name="fundo_upload" style="display: none;" accept="image/jpeg, image/png">
+                    <input type="file" id="fundo_upload" name="fundo_upload" style="display: none;"
+                        accept="image/jpeg, image/png">
 
                     <!-- Adicione um campo de entrada de arquivo -->
-                    <input type="file" id="foto_perfil_upload" name="foto_perfil_upload" style="display: none;" accept="image/jpeg, image/png">
+                    <input type="file" id="foto_perfil_upload" name="foto_perfil_upload" style="display: none;"
+                        accept="image/jpeg, image/png">
 
                     <!-- Div de Foto de Perfil -->
                     <div class="divFotoDePerfil" id="divFotoDePerfil" style="position: relative;">
                         <!-- Div para exibir a imagem de perfil -->
-                        <div id="preview_container" style="position: absolute; top: 0; left: 0; width: 100%; height: 100%; background-size: cover; background-position: center;border-radius: 50%">
+                        <div id="preview_container"
+                            style="position: absolute; top: 0; left: 0; width: 100%; height: 100%; background-size: cover; background-position: center;border-radius: 50%">
                         </div>
                         <!-- Div para o ícone de edição -->
                         <div class="divIconeEditar">
-                            <lord-icon src="https://cdn.lordicon.com/wuvorxbv.json" trigger="hover" stroke="bold" colors="primary:#f5f5f5,secondary:#f5f5f5" style="width:110px;height:110px">
+                            <lord-icon src="https://cdn.lordicon.com/wuvorxbv.json" trigger="hover" stroke="bold"
+                                colors="primary:#f5f5f5,secondary:#f5f5f5" style="width:110px;height:110px">
                             </lord-icon>
                         </div>
                     </div>
@@ -125,14 +132,16 @@ if ($result && mysqli_num_rows($result) > 0) {
                         <div class="inputsLadoALado">
                             <div class="containerInput">
                                 <div class="contentInput">
-                                    <input class="inputAnimado" id="nome" name="nome" type="text" required value="<?php echo $nomeEmpresa; ?>">
+                                    <input class="inputAnimado" id="nome" name="nome" type="text" required
+                                        value="<?php echo $nomeEmpresa; ?>">
                                     <div class="labelLine">Nome da Empresa</div>
                                 </div>
                                 <small name="aviso"></small>
                             </div>
                             <div class="containerInput">
                                 <div class="contentInput">
-                                    <input class="inputAnimado" id="area" name="area" type="text" list="areaList" required value="<?php echo $areaEmpresa; ?>">
+                                    <input class="inputAnimado" id="area" name="area" type="text" list="areaList"
+                                        required value="<?php echo $areaEmpresa; ?>">
                                     <div class="labelLine">Área da Empresa</div>
                                     <datalist id="areaList">
                                         <option>Tecnologia da Informação</option>
@@ -148,7 +157,8 @@ if ($result && mysqli_num_rows($result) > 0) {
                         <div class="inputsLadoALado">
                             <div class="containerInput">
                                 <div class="contentInput">
-                                    <input class="inputAnimado" id="email" name="email" type="text" required value="<?php echo $emailUsuario; ?>">
+                                    <input class="inputAnimado" id="email" name="email" type="text" required
+                                        value="<?php echo $emailUsuario; ?>">
                                     <div class="labelLine">Email</div>
                                 </div>
                                 <small id="aviso" name="aviso" style="display: none;">Caso altere o email é necessário
@@ -156,7 +166,8 @@ if ($result && mysqli_num_rows($result) > 0) {
                             </div>
                             <div class="containerInput">
                                 <div class="contentInput">
-                                    <input class="inputAnimado" maxlength="15" id="telefone" name="telefone" type="text" required value="<?php echo $telefoneEmpresa; ?>">
+                                    <input class="inputAnimado" maxlength="15" id="telefone" name="telefone" type="text"
+                                        required value="<?php echo $telefoneEmpresa; ?>">
                                     <div class="labelLine">Telefone</div>
                                 </div>
                                 <small name="aviso"></small>
@@ -165,14 +176,16 @@ if ($result && mysqli_num_rows($result) > 0) {
                         <div class="inputsLadoALado">
                             <div class="containerInput">
                                 <div class="contentInput">
-                                    <input class="inputAnimado" id="facebook" name="facebook" type="text" value="<?php echo $facebookEmpresa; ?>">
+                                    <input class="inputAnimado" id="facebook" name="facebook" type="text"
+                                        value="<?php echo $facebookEmpresa; ?>">
                                     <div class="labelLine">Facebook</div>
                                 </div>
                                 <small name="aviso"></small>
                             </div>
                             <div class="containerInput">
                                 <div class="contentInput">
-                                    <input class="inputAnimado" id="github" name="github" type="text" value="<?php echo $githubEmpresa; ?>">
+                                    <input class="inputAnimado" id="github" name="github" type="text"
+                                        value="<?php echo $githubEmpresa; ?>">
                                     <div class="labelLine">GitHub</div>
                                 </div>
                                 <small name="aviso"></small>
@@ -181,14 +194,16 @@ if ($result && mysqli_num_rows($result) > 0) {
                         <div class="inputsLadoALado">
                             <div class="containerInput">
                                 <div class="contentInput">
-                                    <input class="inputAnimado" id="linkedin" name="linkedin" type="text" value="<?php echo $linkedinEmpresa; ?>">
+                                    <input class="inputAnimado" id="linkedin" name="linkedin" type="text"
+                                        value="<?php echo $linkedinEmpresa; ?>">
                                     <div class="labelLine">LinkedIn</div>
                                 </div>
                                 <small name="aviso"></small>
                             </div>
                             <div class="containerInput">
                                 <div class="contentInput">
-                                    <input class="inputAnimado" id="instagram" name="instagram" type="text" value="<?php echo $instagramEmpresa; ?>">
+                                    <input class="inputAnimado" id="instagram" name="instagram" type="text"
+                                        value="<?php echo $instagramEmpresa; ?>">
                                     <div class="labelLine">Instagram</div>
                                 </div>
                                 <small name="aviso"></small>
@@ -197,7 +212,8 @@ if ($result && mysqli_num_rows($result) > 0) {
                         <div class="divTextArea">
                             <div class="containerTextArea">
                                 <div class="contentInputTextArea">
-                                    <textarea class="textAreaAnimada" name="sobre" id="sobre" type="text" required><?php echo $sobreEmpresa; ?></textarea>
+                                    <textarea class="textAreaAnimada" name="sobre" id="sobre" type="text"
+                                        required><?php echo $sobreEmpresa; ?></textarea>
                                     <div class="textArealabelLine">Sobre a Empresa</div>
                                 </div>
                                 <small name="aviso"></small>
@@ -208,8 +224,23 @@ if ($result && mysqli_num_rows($result) > 0) {
                         </div>
 
                         <form method="post" action="../../../src/services/ExcluirConta/excluirConta.php">
-                            <div class="divBtnAtualizar">
-                            <a href="../../../src/services/ExcluirConta/excluirConta.php?id=<?php echo $idPessoa; ?>">Excluir Conta</a>
+                            <div style="    text-align: center;
+    margin-top: 20px;
+    cursor: pointer;
+    border: 1px solid #c90000;
+    font-size: 16pt;
+    width: 200px;
+    height: 50px;
+    background-color: #ef0505;
+    color: whitesmoke;
+    border-radius: 14px;
+    transition: 0.2s ease;
+    box-shadow: 0px 0px 8px silver;
+    align-content: center;
+    margin-left: 35%;" onmouseover="this.style.transform='scale(1.1)'" onmouseout="this.style.transform='scale(1)'">
+                                <a
+                                    href="../../../src/services/ExcluirConta/excluirConta.php?id=<?php echo $idPessoa; ?>">Excluir
+                                    Conta</a>
                             </div>
                         </form>
 
@@ -247,19 +278,19 @@ if ($result && mysqli_num_rows($result) > 0) {
         ?>
 
         <script>
-            $(document).ready(function() {
+            $(document).ready(function () {
                 // Adiciona um evento de clique ao botão de edição de foto de perfil
-                $('#divFotoDePerfil').click(function() {
+                $('#divFotoDePerfil').click(function () {
                     $('#foto_perfil_upload').click(); // Simula o clique no campo de upload de foto de perfil
                 });
 
                 // Evento disparado quando um arquivo é selecionado para a foto de perfil
-                $('#foto_perfil_upload').change(function() {
+                $('#foto_perfil_upload').change(function () {
                     var file = this.files[0];
                     var reader = new FileReader();
 
                     // Define o que fazer quando o arquivo é lido
-                    reader.onload = function(e) {
+                    reader.onload = function (e) {
                         $('#preview_container').css('background-image', 'url(' + e.target.result + ')'); // Define a imagem de fundo com a imagem carregada
                         $('#preview_container').show(); // Exibe o contêiner de pré-visualização
                     };
@@ -280,7 +311,7 @@ if ($result && mysqli_num_rows($result) > 0) {
         $urlImagemFundo = $dadosEmpresa['Img_Banner'];
         ?>
         <script>
-            $(document).ready(function() {
+            $(document).ready(function () {
                 // Função para carregar a imagem de fundo com base na URL fornecida
                 function carregarImagemDeFundo(url) {
                     $('.divBackgroundImg').css('background-image', 'url(' + url + ')');
@@ -290,19 +321,19 @@ if ($result && mysqli_num_rows($result) > 0) {
                 carregarImagemDeFundo('<?php echo $urlImagemFundo; ?>');
 
                 // Adiciona um evento de clique ao botão de edição do fundo
-                $('.btnEditarFundo').click(function() {
+                $('.btnEditarFundo').click(function () {
                     // Simula o clique no campo de upload de fundo
                     $('#fundo_upload').click();
                 });
 
                 // Evento disparado quando um arquivo é selecionado
-                $('#fundo_upload').change(function() {
+                $('#fundo_upload').change(function () {
                     // Lê o arquivo selecionado
                     var file = this.files[0];
                     var reader = new FileReader();
 
                     // Define o que fazer quando o arquivo é lido
-                    reader.onload = function(e) {
+                    reader.onload = function (e) {
                         // Aplica a imagem de fundo à div
                         carregarImagemDeFundo(e.target.result);
                     };
